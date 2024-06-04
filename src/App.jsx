6984cfb14889640e13c1cb4272c996931e2569d1
Antonio Romero <tonicb155@hotmail.com>
2024-06-04 20:45:45 +0200

    mitad hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import CountryDetails from "./pages/CountryDetailsPage";
+import HomePage from "./pages/HomePage";
+import { Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    <div>
+    <Navbar></Navbar>
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path= "/countryId" element={<CountryDetails></CountryDetails>}></Route>
+      </Routes>
+      
+    </div>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,26 @@
-function HomePage() {}
+import {ClimbingBoxLoader} from "react-spinners"
+import { useEffect, useState } from "react"
+
+
+
+const Post = () => {
+    const [post, setPost] = useState
+}
+
+
+
+
+function HomePage() {
+    return (
+        <div className="container" style={{maxHeight: "90vh", overflow: "scroll"}}>
+        <h1 style={{fontSize: "24px"}}>WikiCountries: Your Guide to the World</h1>
+
+        <div className="list-group">
+          <a className="list-group-item list-group-item-action" href="/ABW"
+            >🇦🇼 Aruba</a
+        </div>
+      </div>
+    )
+}
 
 export default HomePage;
